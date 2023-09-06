f60914b5a02d878fdd61aabce0a5400d4b4d39f8
Isaque Martins <isaque.martins.barros@gmail.com>
2023-09-05 21:57:59 -0300

    Projeto 1

---
+++ projeto1/projeto1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Leitura e Impressão de Dados
+    void main() {
+
+    int x;
+
+    printf("Escreva um numero inteiro: ");
+    scanf("%d", &x);
+
+    printf("O numero escolhido foi: %d\n", x);
+}
