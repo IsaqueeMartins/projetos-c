0a73ad4ee3270006603cf328c759d34e18349395
Isaque Martins <isaque.martins.barros@gmail.com>
2023-09-06 22:16:08 -0300

    Projeto 2

---
+++ projeto2/projeto2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main () {
+    float x;
+    
+    printf("Digite seu numero: ");
+    scanf("%g", &x);
+
+    printf("O numero escolhido foi: %g\n", x);
+    return 0;
+}
