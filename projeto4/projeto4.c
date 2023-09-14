0a6acbf8e38ed86b54cfc00986fbb543434437a9
Isaque Martins <isaque.martins.barros@gmail.com>
2023-09-14 16:41:14 -0300

    wip projeto4

---
+++ projeto4/projeto4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    float x, y;
+
+    printf("Digite um número: %f", x);
+    scanf("%f", x);
+
+    y = x*x;
+
+    printf("Seu resultado: %f\n", y);
+
+    return;
+}
